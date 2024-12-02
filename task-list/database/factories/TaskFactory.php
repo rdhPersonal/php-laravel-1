86aeb5865fc81ac9efa3b8e8eabeef7cb1bdfd8c
rdhPersonal <richard.harwood.jr@gmail.com>
2024-12-01 19:38:12 -0800

    checkpoint

--- task-list/database/factories/TaskFactory.php
+++ task-list/database/factories/TaskFactory.php
@@ -17,7 +17,10 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence,
+            'description' => fake()->paragraph,
+            'long_description' => fake()->paragraph(7, true),
+            'completed' => fake()->boolean
         ];
     }
 }
