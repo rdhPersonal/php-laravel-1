12b366bb25cc2572ecfab5972f96a94f2f431445
rdhPersonal <richard.harwood.jr@gmail.com>
2024-12-01 20:25:32 -0800

    checkpoint

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -1,11 +1,58 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+// test route with no dependencies
 Route::get('/foo', function () {
     return 'BAR!';
 });
 
+/* default with laravel app create
 Route::get('/', function () {
     return view('welcome');
+}); */
+
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
+
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => \App\Models\Task::latest()->get()
+    ]);
+})->name('tasks.index');
+
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', [
+        'task' => \App\Models\Task::findOrFail($id)
+    ]);
+})->name('tasks.show');
+
+// Route::get('/xxx', function () {
+//     return 'Hello';
+// })->name('hello');
+
+// Route::get('/hallo', function () {
+//     return redirect()->route('hello');
+// });
+
+// Route::get('/greet/{name}', function ($name) {
+//     return 'Hello ' . $name . '!';
+// });
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
 });
