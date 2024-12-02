cd7caebfb95a404fa455ee13502b20c56e13b97a
rdhPersonal <richard.harwood.jr@gmail.com>
2024-12-01 19:34:30 -0800

    checkpoint

--- task-list/database/seeders/DatabaseSeeder.php
+++ task-list/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(10)->create();
+        \App\Models\Task::factory(20)->create();
 
         User::factory()->create([
             'name' => 'Test User 2',
