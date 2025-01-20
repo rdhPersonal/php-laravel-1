3997f8395095167436ba2d41a8d6055e8a8ac741
rdhPersonal <richard.harwood.jr@gmail.com>
2025-01-19 19:18:49 -0800

    checkpoint

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -54,6 +54,12 @@ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
+
 
 // Route::get('/xxx', function () {
 //     return 'Hello';
